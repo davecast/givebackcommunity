472bcba62c4d9c87530e2633dd27e0ca625b0a4c
David Castillo <davecas26@gmail.com>
2024-01-25 23:52:09 -0400

    [modified] add new slide for home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
-const testimonialWrapper = document.querySelector(".testimonial_wrapper");
-let msnryTestimonial = new Masonry(testimonialWrapper, {
-  // options
-  itemSelector: ".testimonial_item",
-  columnWidth: 469,
-  gutter: 30,
+document.addEventListener("DOMContentLoaded", function () {
+  const testimonialWrapper = document.querySelector(".testimonial_wrapper");
+  const splideHome = document.getElementById("splideHome");
+
+  let msnryTestimonial = new Masonry(testimonialWrapper, {
+    // options
+    itemSelector: ".testimonial_item",
+    columnWidth: 469,
+    gutter: 30,
+  });
+
+  if (!!splideHome) {
+    var splide = new Splide("#splideHome", {
+      type: "loop",
+      drag: "free",
+      snap: true,
+      perPage: 3,
+      pagination: false,
+      autoplay: true,
+      focus: 0,
+      omitEnd: true,
+    });
+
+    splide.mount();
+  }
 });
